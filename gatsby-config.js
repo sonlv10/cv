62d12e53e38c094e87c0ee05a3febf75c0059c0a
sonle <son.le@hdwebsoft.co>
2021-06-12 19:03:07 +0700

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  siteMetadata: {
+    description: "Personal page of John Doe",
+    locale: "en",
+    title: "John Doe",
+    formspreeEndpoint: "https://formspree.io/f/{your-id}",
+  },
+  plugins: [
+    {
+      resolve: "@wkocjan/gatsby-theme-intro",
+      options: {
+        basePath: "/",
+        contentPath: "content/",
+        showThemeLogo: true,
+        theme: "classic",
+      },
+    },
+  ],
+}
